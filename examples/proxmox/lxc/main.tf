2c5d0e55084b1eb420601942d632e71a1c6fe8fa
Yaya DIALLO <yayamombeya090@gmail.com>
2025-08-21 23:06:57 -0400

    feat: update default template_name for LxC Container and remove download_image comment

--- examples/proxmox/lxc/main.tf
+++ examples/proxmox/lxc/main.tf
@@ -1,8 +1,7 @@
 module "lxc" {
-  source         = "../../../src/proxmox/lxc"
-  hostname       = "my-test-container"
-  id             = 1234
-  ip_address     = "192.168.10.123"
-  password       = "test123"
-  download_image = false #to true if image is not already downloaded
+  source     = "../../../src/proxmox/lxc"
+  hostname   = "my-test-container"
+  id         = 1234
+  ip_address = "192.168.10.123"
+  password   = "test123"
 }
