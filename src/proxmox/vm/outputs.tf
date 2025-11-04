94ff759b523ed5df10b8dd137d143cc4df771178
Mamadou Yaya DIALLO <yayamombeya090@gmail.com>
2025-11-03 22:04:35 -0500

    wip

--- src/proxmox/vm/outputs.tf
+++ src/proxmox/vm/outputs.tf
@@ -1,39 +1,39 @@
 output "vm_id" {
-  value       = proxmox_virtual_environment_vm.this[0].id
+  value       = try(proxmox_virtual_environment_vm.this[0].id, null)
   description = "ID of the VM"
 }
 
 output "vm_name" {
-  value       = proxmox_virtual_environment_vm.this[0].name
+  value       = try(proxmox_virtual_environment_vm.this[0].name, null)
   description = "Name of the VM"
 }
 
 output "agent" {
-  value       = proxmox_virtual_environment_vm.this[0].agent[0].enabled
+  value       = try(proxmox_virtual_environment_vm.this[0].agent[0].enabled, null)
   description = "Agent enabled"
 }
 
 output "vm_cpu_count" {
-  value       = proxmox_virtual_environment_vm.this[0].cpu[0].cores
+  value       = try(proxmox_virtual_environment_vm.this[0].cpu[0].cores, null)
   description = "Number of CPU cores"
 }
 
 output "vm_memory" {
-  value       = proxmox_virtual_environment_vm.this[0].memory[0].dedicated
+  value       = try(proxmox_virtual_environment_vm.this[0].memory[0].dedicated, null)
   description = "Amount of memory allocated to the VM"
 }
 
 output "vm_disk_size" {
-  value       = "${proxmox_virtual_environment_vm.this[0].disk[0].size} Go"
+  value       = try("${proxmox_virtual_environment_vm.this[0].disk[0].size} Go", null)
   description = "Size of the VM disk"
 }
 
 output "network_config" {
   value = {
-    ip_address     = proxmox_virtual_environment_vm.this[0].initialization[0].ip_config[0].ipv4[0].address
-    gateway        = proxmox_virtual_environment_vm.this[0].initialization[0].ip_config[0].ipv4[0].gateway
-    search_domains = proxmox_virtual_environment_vm.this[0].initialization[0].dns[0].domain
-    servers        = proxmox_virtual_environment_vm.this[0].initialization[0].dns[0].servers
+    ip_address     = try(proxmox_virtual_environment_vm.this[0].initialization[0].ip_config[0].ipv4[0].address, null)
+    gateway        = try(proxmox_virtual_environment_vm.this[0].initialization[0].ip_config[0].ipv4[0].gateway, null)
+    search_domains = try(proxmox_virtual_environment_vm.this[0].initialization[0].dns[0].domain, null)
+    servers        = try(proxmox_virtual_environment_vm.this[0].initialization[0].dns[0].servers, null)
   }
   description = "Network configuration of the VM"
 }
