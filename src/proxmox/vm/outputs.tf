6dba066712a34977d2f771e5b6b987802a06cea9
Mamadou Yaya DIALLO <yayamombeya090@gmail.com>
2025-11-03 21:55:54 -0500

    wip

--- src/proxmox/vm/outputs.tf
+++ src/proxmox/vm/outputs.tf
@@ -1,39 +1,39 @@
 output "vm_id" {
-  value       = proxmox_virtual_environment_vm.this.id
+  value       = proxmox_virtual_environment_vm.this[0].id
   description = "ID of the VM"
 }
 
 output "vm_name" {
-  value       = proxmox_virtual_environment_vm.this.name
+  value       = proxmox_virtual_environment_vm.this[0].name
   description = "Name of the VM"
 }
 
 output "agent" {
-  value       = proxmox_virtual_environment_vm.this.agent[0].enabled
+  value       = proxmox_virtual_environment_vm.this[0].agent[0].enabled
   description = "Agent enabled"
 }
 
 output "vm_cpu_count" {
-  value       = proxmox_virtual_environment_vm.this.cpu[0].cores
+  value       = proxmox_virtual_environment_vm.this[0].cpu[0].cores
   description = "Number of CPU cores"
 }
 
 output "vm_memory" {
-  value       = proxmox_virtual_environment_vm.this.memory[0].dedicated
+  value       = proxmox_virtual_environment_vm.this[0].memory[0].dedicated
   description = "Amount of memory allocated to the VM"
 }
 
 output "vm_disk_size" {
-  value       = "${proxmox_virtual_environment_vm.this.disk[0].size} Go"
+  value       = "${proxmox_virtual_environment_vm.this[0].disk[0].size} Go"
   description = "Size of the VM disk"
 }
 
 output "network_config" {
   value = {
-    ip_address     = proxmox_virtual_environment_vm.this.initialization[0].ip_config[0].ipv4[0].address
-    gateway        = proxmox_virtual_environment_vm.this.initialization[0].ip_config[0].ipv4[0].gateway
-    search_domains = proxmox_virtual_environment_vm.this.initialization[0].dns[0].domain
-    servers        = proxmox_virtual_environment_vm.this.initialization[0].dns[0].servers
+    ip_address     = proxmox_virtual_environment_vm.this[0].initialization[0].ip_config[0].ipv4[0].address
+    gateway        = proxmox_virtual_environment_vm.this[0].initialization[0].ip_config[0].ipv4[0].gateway
+    search_domains = proxmox_virtual_environment_vm.this[0].initialization[0].dns[0].domain
+    servers        = proxmox_virtual_environment_vm.this[0].initialization[0].dns[0].servers
   }
   description = "Network configuration of the VM"
 }
